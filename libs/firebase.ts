f8a8fae65b3e807212d99306ec98e52a68a5f183
EdenBar01 <edenbar2008@gmail.com>
2024-03-29 17:01:08 +0300

    firebase

---
+++ libs/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbHfrVeijyx294VLmj0aYwAuP_uqvmUIU",
+  authDomain: "frame-of-fame.firebaseapp.com",
+  projectId: "frame-of-fame",
+  storageBucket: "frame-of-fame.appspot.com",
+  messagingSenderId: "200436645471",
+  appId: "1:200436645471:web:a744756be4e04b3f80a5ea"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
