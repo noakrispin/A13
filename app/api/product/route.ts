36323ca333d986e64c529a3c69f74f70377fcaf2
EdenBar01 <edenbar2008@gmail.com>
2024-03-30 12:05:22 +0300

    started admin add pages

---
+++ app/api/product/route.ts
@@ -0,0 +1,16 @@
+import prisma from '@/libs/prismadb'
+import { NextResponse } from 'next/server'
+
+// const body = await request.json();
+// const {name, description, price, size, artistName, category, inStock, image} = body;
+
+// const product = await prisma.product.create({
+//  name,
+//  description,
+//  category,
+//  artistName,
+//  size,
+//  inStock,
+//  image,
+//  price: parseFloat(price)
+// });
