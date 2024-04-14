321d85b32501e66e65ce435ce189b2442fbcffcb
noakrispin <74138457+noakrispin@users.noreply.github.com>
2024-04-14 20:44:45 +0300

    fix for npm run biuld

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default async function Home({searchParams}: HomeProps) {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 mt-6">
           {shuffledProducts.map((product: any) => (
-            <ProductsCard data={product} />
+            <ProductsCard key={product.id} data={product} />
           ))}
         </div>
 
