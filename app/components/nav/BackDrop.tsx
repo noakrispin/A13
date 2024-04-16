4e16f93e3eb8da3dc24ef6bb7d2927847581d230
noakrispin <74138457+noakrispin@users.noreply.github.com>
2024-04-16 17:23:28 +0300

    fix home page

--- app/components/nav/BackDrop.tsx
+++ app/components/nav/BackDrop.tsx
@@ -2,25 +2,25 @@
 
 // Define the props interface for BackDrop component
 interface BackDropProps{
-    onClick: () => void; 
+  onClick: () => void; 
 }
 
 // BackDrop component to render a backdrop that closes on click
 const BackDrop: React.FC<BackDropProps> = ({onClick}) => {
-  return (
-    <div onClick={onClick} className="
-    z-20
-    bg-slate-200
-    opacity-50
-    w-screen
-    h-screen
-    fixed
-    top-0
-    left-0
-    ">
+return (
+  <div onClick={onClick} className="
+  z-20
+  bg-slate-200
+  opacity-50
+  w-screen
+  h-screen
+  fixed
+  top-0
+  left-0
+  ">
 
-    </div>
-  );
+  </div>
+);
 };
 
 export default BackDrop;
